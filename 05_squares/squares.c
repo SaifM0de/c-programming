8742a46356d106ac8a479e9721aef16fb4173aa8
Coursera Learner <nobody@nowhere.nul>
2020-05-27 13:43:42 +0000

    Did squares.c

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -6,25 +6,63 @@
  * Determines if coord is in range between
  * offset (INCLUSIVE) and offset + size (EXCLUSIVE)
  */
-int isInRange(int coord, int offset, int size) {
-  // if coord is in range, return 1
+int isInRange(int coord, int offset, int size){
+  if ((coord >= offset) & (coord < (offset + size))){
+    return 1;
+  }
+  else{
+    return 0;
+  }// if coord is in range, return 1
   // else, return 0
-  return 0;
-}
+ }
 
 /*
  * Determines if coord is at border of offset or
  * offset + size
  */
-int isAtBorder(int coord, int offset, int size) {
-  // if coord is equal to offest or offset + size
+int isAtBorder(int coord, int offset, int size){
+  if ((coord == offset) | (coord == (offset + size))){
+    return 1;
+  }
+  else{
+    return 0;
+  } // if coord is equal to offest or offset + size
   // return 1, else return 0
-  return 0;
+
 }
 
-void squares(int size1, int x_offset, int y_offset, int size2) {
-  //compute the max of size1 and (x_offset + size2).  Call this w
+void squares(int size1, int x_offset, int y_offset, int size2){
+  int w, h;
+  if (size1 > (x_offset + size2)){
+    w = size1;
+  }
+  else{
+    w = x_offset + size2;
+  }//compute the max of size1 and (x_offset + size2).  Call this w
+  if (size1 > (y_offset + size2)){
+    h = size1;
+  }
+  else{
+    h = y_offset + size2;
+  }
 
+  for (int y = 0; y < h; y++){
+    for (int x = 0; x < w; x++){
+      if (((isInRange(x, x_offset, size2)) & (isAtBorder(y, y_offset, size2 -1))) | ((isInRange(y, y_offset, size2)) & (isAtBorder(x, x_offset, size2 -1)))){
+	printf("*");
+      }
+      else if (((x < size1) & ((y == 0) | (y == size1 -1))) | ((y < size1) & ((x == 0) | (x == size1 -1)))){
+	printf("#");
+      }
+      else {
+	printf(" ");
+      }
+    }
+    printf("\n");
+  }
+}
+      
+      
   //compute the max of size1 and (y_offset + size2).  Call this h
 
   //count from 0 to h. Call the number you count with y
@@ -50,4 +88,3 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
     //when you finish counting x from 0 to w, 
     //print a newline
 
-}
